541a6df41c2a71ee29e9b096bab09b76d8b5844d
gv4383 <gregory.vargas18@gmail.com>
2021-10-11 22:21:37 -0500

    Initial Commit

---
+++ Converty/ConvertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertyApp.swift
+//  Converty
+//
+//  Created by Goyo Vargas on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
