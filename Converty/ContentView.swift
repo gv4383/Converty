61b20ded450432c554ca7e3f16cce05ed7ed3a75
gv4383 <gregory.vargas18@gmail.com>
2021-10-11 23:24:23 -0500

    Initial commit

--- Converty/ContentView.swift
+++ Converty/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var originalMeasurement = ""
+    @State private var originalMeasurementUnit = 1
+    @State private var convertedMeasurementUnit = 2
+    
+    let temperatureUnits = ["K", "C", "F"]
+    
+    var convertedMeasurement: Double {
+        let originalMeasurementDouble = Double(originalMeasurement) ?? 0
+        
+        if temperatureUnits[convertedMeasurementUnit] == temperatureUnits[originalMeasurementUnit] {
+            return originalMeasurementDouble
+        }
+        
+        var convertedMeasurementToKelvin = originalMeasurementDouble
+        
+        if temperatureUnits[originalMeasurementUnit] == "C" {
+            let convertedCelsiusToKelvin = originalMeasurementDouble + 273.15
+            convertedMeasurementToKelvin = convertedCelsiusToKelvin
+        }
+        
+        if temperatureUnits[originalMeasurementUnit] == "F" {
+            let convertedFahrenheitToKelvin = (originalMeasurementDouble - 32) * 5 / 9 + 273.15
+            
+            convertedMeasurementToKelvin = convertedFahrenheitToKelvin
+        }
+        
+        var finalConvertedMeasurement = convertedMeasurementToKelvin
+        
+        if temperatureUnits[convertedMeasurementUnit] == "C" {
+            let convertedKelvinToCelsius = convertedMeasurementToKelvin - 273.15
+            finalConvertedMeasurement = convertedKelvinToCelsius
+        }
+        
+        if temperatureUnits[convertedMeasurementUnit] == "F" {
+            let convertedKelvinToFahrenheit = (convertedMeasurementToKelvin - 273.15) * 9 / 5 + 32
+            finalConvertedMeasurement = convertedKelvinToFahrenheit
+        }
+        
+        return finalConvertedMeasurement
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Original measurement and unit")) {
+                    TextField("Temperature", text: $originalMeasurement)
+                        .keyboardType(.decimalPad)
+                    Picker("Original measurement unit", selection: $originalMeasurementUnit) {
+                        ForEach(0 ..< temperatureUnits.count) {
+                            Text("\(self.temperatureUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Unit to convert to")) {
+                    Picker("Converted measurement unit", selection: $convertedMeasurementUnit) {
+                        ForEach(0 ..< temperatureUnits.count) {
+                            Text("\(self.temperatureUnits[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Converted measurement")) {
+                    Text("\(convertedMeasurement, specifier: "%.1f") \(temperatureUnits[convertedMeasurementUnit])")
+                }
+            }
+            .navigationBarTitle("Converty")
+        }
     }
 }
 
